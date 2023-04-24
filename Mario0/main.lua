0b61fa02e09b2176df72a16fd23e8e67a458563b
tup85417 <129218310+tup85417@users.noreply.github.com>
2023-04-24 03:05:27 +0000

    Latest Changes

--- Mario0/main.lua
+++ Mario0/main.lua
@@ -1,32 +1,39 @@
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+window_width = 1280
+window_height = 720
 
-VIRTUAL_WIDTH = 432
-VIRTUAL_HEIGHT = 243
+virtual_width = 432
+virtual_heigth = 243
 
-Class = require 'class'
+class = require 'class'
 push = require 'push'
 
+require 'Util'
 require 'Map'
 
 function love.load()
+
     map = Map()
 
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    push:setupScreen(virtual_width, virtual_heigth, window_width, window_height, {
     fullscreen = false,
-    resizeable = false,
+    resizable = false,
     vsynce = true
-    })
+    }
+)
+
 end
 
 function love.update(dt)
-
+    Map:update(dt)
 end
 
 function love.draw()
+
     push:apply('start')
-    love.graphics.clear(108/255,140/255,1,1)
-    love.graphics.print('Hello, World!')
+    love.graphics.clear(108/255, 140/255, 255/255, 255/255)
+    
+    love.graphics.translate(-map.camX, -map.camY)
     map:render()
     push:apply('end')
+
 end
